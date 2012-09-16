03ae0b391bebe5513a984b5a97a53921a06aab0b
Marian <info@hackordnung.net>
2012-09-16 22:50:25 +0200

    Shows doc for word only for javascript

--- init.js
+++ init.js
@@ -1,8 +1,23 @@
+var dochub_url = "http://dochub.io";
+
 Hooks.addMenuItem("Go/Search on dochub.io", "cmd-ctrl-h", function () {
 	Recipe.run(function(r) {
 		var wordrange = r.wordRangeForRange(r.selection);
 		var word = r.textInRange(wordrange);	
 		
-		Alert.show("This " + word + " Dochub.io");
+		showPopover(word, wordrange);
+		
 	});
 });
+
+function showPopover (word, wordPosition) {
+	var popover = new Popover(Editor.current(), wordPosition);
+	
+	var currentLanguage = "javascript";
+	
+	popover.size = {width: 800, height: 500};
+	
+	popover.url = dochub_url + "/#" + currentLanguage + "/" + word;
+	
+	popover.run();
+}
