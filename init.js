e4fb736af60f4c0b48cbd16258174779cf6d90d4
Marian <info@hackordnung.net>
2012-09-18 23:25:03 +0200

    Add dynamic language from the opened file.

--- init.js
+++ init.js
@@ -13,11 +13,36 @@ Hooks.addMenuItem("Go/Search on dochub.io", "cmd-ctrl-h", function () {
 function showPopover (word, wordPosition) {
 	var popover = new Popover(Editor.current(), wordPosition);
 	
-	var currentLanguage = "javascript";
+	var currentLanguage = getLanguage();
 	
-	popover.size = {width: 800, height: 500};
+	if(currentLanguage == "unkown") {
+		Alert.show("Unkown Language", "This language is not kown by dochub.io");
+	} else {
+		popover.size = {width: 800, height: 500};	
+		popover.url = dochub_url + "/#" + currentLanguage + "/" + word;	
+		popover.run();	
+	}
+}
+
+function getLanguage () {
+	var scope = Document.current().rootScope();
 	
-	popover.url = dochub_url + "/#" + currentLanguage + "/" + word;
+	var knownLanguages = ["js", "php", "css", "html"];
 	
-	popover.run();
+	var array = scope.split(".");
+	var scopeLanguage = "unkown";
+	
+	knownLanguages.forEach(function(element, index, array) {
+		var indexOfResult = array.indexOf(element);
+		Alert.show("Debug", element);
+		
+		if(indexOfResult > -1) {
+			scopeLanguage = element;
+		}
+	});
+	
+	//@TODO: if scope is js also search in dom and jquery
+	if(scopeLanguage == "js") scopeLanguage = "javascript";
+	
+	return scopeLanguage;
 }
